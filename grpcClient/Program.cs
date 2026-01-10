5eaf3b86f225a3a311abd5df9685d8488bacc0cc
Muhammet Esat Demir <muhammetesatdemir@outlook.com>
2026-01-10 17:05:04 +0300

    ilk haberleşme eklendi

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using grpcClient;
+
+
+var channel = GrpcChannel.ForAddress("http://localhost:5127");
+var greetingClient = new Greeter.GreeterClient(channel);
+ HelloReply result =await greetingClient.SayHelloAsync(
+     new HelloRequest 
+     { Name = " World" 
+     });
+
+Console.WriteLine(result.Message);
+Console.ReadLine();

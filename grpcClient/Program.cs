34d66d38f3db215cf326d19781eb03d9c372977e
Muhammet Esat Demir <muhammetesatdemir@outlook.com>
2026-01-12 13:05:14 +0300

    Bidirectional streamingin eklenmesi

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,7 +1,9 @@
-﻿using Grpc.Core;
+﻿using System.IO;
+using Grpc.Core;
 using Grpc.Net.Client;
 using grpcAlertClient;
 using grpcClient;
+using grpcErrorClient;
 using grpcMessageClient;
 
 ///Unary
@@ -34,6 +36,10 @@ _ = Task.Run(async () =>
     }
 });
 
+
+// =====================
+// Client STREAMING (arka planda)
+// =====================
 var alertresponseClient = new Alert.AlertClient(channel);
 var requestStream = alertresponseClient.SendAlert();
 for (int i = 0; i < 10; i++)
@@ -48,4 +54,34 @@ for (int i = 0; i < 10; i++)
 await requestStream.RequestStream.CompleteAsync();
 var response = await requestStream.ResponseAsync;
 Console.WriteLine("Server says: " + response.Message);
+
+
+// =====================
+// Bidirectional STREAMING (arka planda)
+// =====================
+var errorClient = new Error.ErrorClient(channel);
+var errorStream = errorClient.SendError();
+Random rnd = new Random();
+_ = Task.Run(async () =>
+{
+    await foreach (var msg in errorStream.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine("FROM SERVER: " + msg.Error);
+    }
+});
+for (int i = 0; i < 20; i++)
+{
+    int value = rnd.Next(0, 100);
+
+    await errorStream.RequestStream.WriteAsync(new ErrorRequest
+    {
+        Error="Client error Value: " + value
+    });
+
+    Console.WriteLine("RANDOM TO SERVER => " + value);
+
+    await Task.Delay(1000);
+}
+
+await errorStream.RequestStream.CompleteAsync();
 Console.ReadLine();
---
+++ grpcServer/Services/ErrorService.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using grpcErrorServer;
+
+namespace grpcServer.Services
+{
+    public class ErrorService : Error.ErrorBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public ErrorService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+        public override async Task SendError(IAsyncStreamReader<ErrorRequest> requestStream, IServerStreamWriter<ErrorResponse> responseStream, ServerCallContext context)
+        {
+            var rnd = new Random();
+            await foreach (var v in requestStream.ReadAllAsync())
+            {
+
+                Console.WriteLine("CLIENT error -> " + v.Error);
+
+                var serverValue = rnd.Next(0, 1000);
+
+                await responseStream.WriteAsync(new ErrorResponse
+                {
+                    Error = "Server error Value: " + serverValue
+                });
+
+            
+
+            }
+        }
+    }
+}
